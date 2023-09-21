b1aaebdd6e38a7ee511c9e86412ec72531067761
Annacheer <125766262+Annacheer@users.noreply.github.com>
2023-09-20 23:32:16 -0400

    second commit

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Modal extends Component {
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      this.props.onClose();
+    }
+  };
+
+  handleOverlayClick = e => {
+    if (e.target === e.currentTarget) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    return (
+      <div className="Overlay" onClick={this.handleOverlayClick}>
+        <div className="Modal">
+          <img src={this.props.src} alt={this.props.alt} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Modal;
